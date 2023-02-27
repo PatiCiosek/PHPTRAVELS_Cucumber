d5e2403899582ee02306166a84366efb5a42dd1a
patrycja.ciosek <patrycja.ciosek@ten10.com>
2023-02-27 17:03:24 +0000

    Created a Test Runner for all the tests

---
+++ PHPTRAVELS/src/test/java/TestRunners/TestRunner.java
@@ -0,0 +1,11 @@
+package TestRunners;
+
+import io.cucumber.junit.Cucumber;
+import io.cucumber.junit.CucumberOptions;
+import org.junit.runner.RunWith;
+
+@RunWith(Cucumber.class)
+@CucumberOptions(features= "src/test/resources/Features", glue = {"StepDefinitions"}, plugin = {"json:target/cucumber-report/cucumber.json"})
+public class TestRunner {
+
+}
